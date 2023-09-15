e31fe6b2b3868b1ba369befd0ceb8c90c96d314d
Alisson Rodrigo <alisson.rodrigo1307@gmail.com>
2023-09-14 22:18:43 -0300

    refatoração validações

---
+++ src/validation/loginSchema.js
@@ -0,0 +1,15 @@
+const joi = require('joi')
+
+const loginSchema = joi.object({
+    email: joi.string().email().required().messages({
+        'any.required': 'O campo nome é obrigatório',
+        'string.empty': 'O campo nome é obrigatório',
+        'string.email': 'Email inválido'
+    }),
+    password: joi.string().required().messages({
+        'any.required': 'O campo senha é obrigatório',
+        'string.empty': 'O campo senha é obrigatório',
+    })
+})
+
+module.exports = loginSchema;

228e0fa5d0abf8c3dd1777631589b9de206b4bf2
sarah <sarah.rendeiro@outlook.com>
2023-09-16 11:58:38 -0300

    alteração no cadastro do cliente

--- src/validation/clientSchema.js
+++ src/validation/clientSchema.js
@@ -1,22 +1,10 @@
 const joi = require("joi");
 
 const clientSchema = joi.object({
-    nome: joi.string().required().messages({
-        "any.required": "O campo nome é obrigatório",
-        "string.empty": "O campo nome é obrigatório",
-    }),
-    email: joi.string().email().required().messages({
-        "any.required": "O campo nome é obrigatório",
-        "string.empty": "O campo nome é obrigatório",
-    }),
-    cpf: joi.string().required().max(11).messages({
-        "any.required": "O campo nome é obrigatório",
-        "string.empty": "O campo nome é obrigatório",
-    }),
-    phone: joi.string().required().max(16).messages({
-        "any.required": "O campo nome é obrigatório",
-        "string.empty": "O campo nome é obrigatório",
-    }),
+    nome: joi.string().required().messages({}),
+    email: joi.string().email().required().messages({}),
+    cpf: joi.string().required().max(11).messages({}),
+    phone: joi.string().required().max(16).messages({}),
     cep: joi.string().required().max(8).messages({}),
     address: joi.string().required().messages({}),
     complement: joi.string().messages({}),

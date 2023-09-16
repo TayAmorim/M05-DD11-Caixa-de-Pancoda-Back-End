1071772167dd35fca527be12d67cbacdd15ecb41
Tayanna Amorim <amorim.tayanna@gmail.com>
2023-09-16 13:01:59 -0300

    fix: ajuste dos nomes do campo do banco de dados de clientes

--- src/controllers/clients.js
+++ src/controllers/clients.js
@@ -1,53 +1,68 @@
-const knex = require('../conection');
-const clientSchema = require('../validation/clientSchema');
+const knex = require("../conection");
+const clientSchema = require("../validation/clientSchema");
 
-const requiredField = ['nome', 'email', 'cpf', 'phone'];
+const requiredField = ["name", "email", "cpf", "phone_client"];
 
 const newClient = async (req, res) => {
-    const { nome, email, cpf, phone, cep, address, complement, neighborhood, city, state } = req.body;
-    const { id } = req.user;
+  const {
+    name,
+    email,
+    cpf,
+    phone_client,
+    cep,
+    address,
+    complement,
+    neighborhood,
+    city,
+    state,
+  } = req.body;
+  const { id } = req.user;
 
-    try {
-        await clientSchema.validate(req.body);
+  try {
+    await clientSchema.validate(req.body);
 
-        const existEmail = await knex('clients').where({ email });
+    const existEmail = await knex("customers").where({ email });
 
-        if (existEmail.length > 0) {
-            return res.status(400).json({ mensagem: 'Email já cadastrado no sistema.' });
-        }
-
-        if (requiredField.some(field => !req.body[field])) {
-            return res.status(400).json({ mensagem: 'Cliente não cadastrado, campo obrigatório' });
-        }
-
-        const client = await knex('clients').insert({
-            id_usuario: id,
-            nome,
-            email,
-            cpf,
-            phone,
-            cep,
-            address,
-            complement,
-            neighborhood,
-            city,
-            state
-        }).returning('*');
-
-        if (client) {
-            return res.status(201).json({ mensagem: 'Cadastro realizado com sucesso!' });
-        }
-
-
-    } catch (error) {
-        if (error.name === 'ValidationError') {
-            const errorMessages = error.errors;
-            console.log(errorMessages[0]);
-            return res.status(400).json(errorMessages[0]);
-        }
-        return res.status(500).json({ mensagem: "Erro interno do servidor" })
+    if (existEmail.length > 0) {
+      return res
+        .status(400)
+        .json({ mensagem: "Email já cadastrado no sistema." });
     }
-};
 
+    if (requiredField.some((field) => !req.body[field])) {
+      return res
+        .status(400)
+        .json({ mensagem: "Cliente não cadastrado, campo obrigatório" });
+    }
+
+    const client = await knex("customers")
+      .insert({
+        id_user: id,
+        name,
+        email,
+        cpf,
+        phone_client,
+        cep,
+        address,
+        complement,
+        neighborhood,
+        city,
+        state,
+      })
+      .returning("*");
+    if (client) {
+      return res
+        .status(201)
+        .json({ mensagem: "Cadastro realizado com sucesso!" });
+    }
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      const errorMessages = error.errors;
+      console.log(errorMessages[0]);
+      return res.status(400).json(errorMessages[0]);
+    }
+    return res.status(500).json({ mensagem: "Erro interno do servidor" });
+  }
+};
 
 module.exports = { newClient };
--- src/validation/clientSchema.js
+++ src/validation/clientSchema.js
@@ -1,17 +1,16 @@
 const joi = require("joi");
 
 const clientSchema = joi.object({
-    nome: joi.string().required().messages({}),
-    email: joi.string().email().required().messages({}),
-    cpf: joi.string().required().max(11).messages({}),
-    phone: joi.string().required().max(16).messages({}),
-    cep: joi.string().required().max(8).messages({}),
-    address: joi.string().required().messages({}),
-    complement: joi.string().messages({}),
-    neighborhood: joi.string().required().messages({}),
-    city: joi.string().required().messages({}),
-    state: joi.string().length(2).required().messages({})
-})
+  name: joi.string().required().messages({}),
+  email: joi.string().email().required().messages({}),
+  cpf: joi.string().required().max(11).messages({}),
+  phone_client: joi.string().required().max(16).messages({}),
+  cep: joi.string().required().max(8).messages({}),
+  address: joi.string().required().messages({}),
+  complement: joi.string().messages({}),
+  neighborhood: joi.string().required().messages({}),
+  city: joi.string().required().messages({}),
+  state: joi.string().length(2).required().messages({}),
+});
 
 module.exports = clientSchema;
-

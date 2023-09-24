825ba69ca9661f41839fd6a92d680082526b11ff
sarah <sarah.rendeiro@outlook.com>
2023-09-24 02:09:12 -0300

    atualizacao do codigo

--- src/controllers/clients.js
+++ src/controllers/clients.js
@@ -1,11 +1,12 @@
 const knex = require("../conection");
 const clientSchema = require("../validation/clientSchema");
+const updateClientSchema = require("../validation/updateClientSchema");
 
 const requiredField = [
   "name_client",
   "email_client",
   "cpf_client",
-  "phone_client"
+  "phone_client",
 ];
 
 const newClient = async (req, res) => {
@@ -55,10 +56,91 @@ const newClient = async (req, res) => {
         state,
       })
       .returning("*");
+
     if (client) {
       return res
         .status(201)
         .json({ mensagem: "Cadastro realizado com sucesso!" });
+    }
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      const errorMessages = error.errors;
+      console.log(errorMessages[0]);
+      return res.status(400).json(errorMessages[0]);
+    }
+    return res.status(500).json({ mensagem: "Erro interno do servidor" });
+  }
+};
+
+const updateClient = async (req, res) => {
+  const {
+    name_client,
+    email_client,
+    cpf_client,
+    phone_client,
+    cep,
+    address,
+    complement,
+    neighborhood,
+    city,
+    state,
+  } = req.body;
+  const { id } = req.user;
+  const { identification } = req.params;
+
+  try {
+    await updateClientSchema.validate(req.body);
+
+    const findClient = await knex("customers").where({ id: identification });
+    const registeredEmail = await knex("customers").where({ email_client });
+    const registeredCpf = await knex("customers").where({ cpf_client });
+
+    if (findClient.length === 0) {
+      return res.status(400).json({ mensagem: "Cliente não encontrado" });
+    }
+
+    if (
+      registeredEmail.length > 0 &&
+      email_client !== findClient[0].email_client
+    ) {
+      return res
+        .status(400)
+        .json({ mensagem: "Email já cadastrado no sistema." });
+    }
+
+    if (registeredCpf.length > 0 && cpf_client !== findClient[0].cpf_client) {
+      return res
+        .status(400)
+        .json({ mensagem: "CPF já cadastrado no sistema." });
+    }
+
+    if (requiredField.some((field) => !req.body[field])) {
+      return res
+        .status(400)
+        .json({
+          mensagem: "Cliente não cadastrado, campo obrigatório em branco.",
+        });
+    }
+
+    const updateClient = await knex("customers")
+      .where({ id: identification })
+      .update({
+        id_user: id,
+        name_client,
+        email_client,
+        cpf_client,
+        phone_client,
+        cep,
+        address,
+        complement,
+        neighborhood,
+        city,
+        state,
+      })
+      .returning("*");
+
+    if (updateClient) {
+      return res.status(200).json(updateClient[0]);
     }
   } catch (error) {
     if (error.name === "ValidationError") {
@@ -77,7 +159,7 @@ const listingClients = async (req, res) => {
     const currentPage = page || 1;
     const offSet = (currentPage - 1) * cutOff;
     const currentDate = new Date();
-    const totalClients = await knex('customers').count('* as total').first();
+    const totalClients = await knex("customers").count("* as total").first();
     const totalPages = Math.ceil(totalClients.total / cutOff);
 
     const clients = await knex("customers")
@@ -107,7 +189,42 @@ const listingClients = async (req, res) => {
   }
 };
 
+const detailClient = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const client = await knex("customers")
+      .select(
+        "name_client",
+        "email_client",
+        "cpf_client",
+        "phone_client",
+        "address",
+        "neighborhood",
+        "complement",
+        "cep",
+        "city",
+        "state"
+      )
+      .where({ id })
+      .first();
+    const charges = await knex("charges")
+      .select("id_charges", "due_date", "amount", "status", "description")
+      .where({ id_customer: id });
+    if (!client) {
+      return res.status(404).json({ mensagem: "Cliente não encontrado" });
+    }
+
+    return res.json({ client, charges });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ mensagem: "Erro interno do servidor" });
+  }
+};
+
 module.exports = {
   newClient,
-  listingClients
+  listingClients,
+  detailClient,
+  updateClient,
 };
